f0870fe0a05194f3a25d2a10f50d947c37c127af
Ryan Gowan <tr_jester@hotmail.com>
2016-01-20 19:00:24 -0500

    1-20-16 update

---
+++ cis237inclass1/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass1
+{
+    class Employee
+    {
+        // Backing Fields
+        private String firstName;
+        private String lastName;
+        private Decimal weeklySalary;
+
+        // properties for the backing fields
+        public String FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public String LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public  Decimal WeeklySalary
+        {
+            get { return weeklySalary; }
+            set { weeklySalary = value; }
+        }
+
+        //One Method that is public. Can be accessed from other classes.
+        public String GetFullName()
+        {
+            return this.firstName + " " + this.lastName;
+        }
+
+        public override string ToString()
+        {
+            return this.firstName + " " + this.lastName + " " + this.weeklySalary.ToString("c");
+        }
+
+        //One Method that is private. Can only be called from inside this class.
+        private String FOO()
+        {
+            return "FOO";
+        }
+
+        //3 Parameter constructor.
+        public Employee (String firstName, String lastName, Decimal weeklySalary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            //Can do weeklySalary the same way,
+            //or,
+            //Assign the passed in value by the property.
+            this.WeeklySalary = weeklySalary;
+        }
+
+        //Default constructor
+        public Employee()
+        {
+            //Lets just leave this blank.
+        }
+    }
+}
--- cis237inclass1/Program.cs
+++ cis237inclass1/Program.cs
@@ -10,10 +10,16 @@ namespace cis237inclass1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Here is some text");
-            Console.WriteLine("Here is some more text");
-            Console.ReadKey();
+            //Console.WriteLine("Here is some text");
+            //Console.WriteLine("Here is some more text");
+            //Console.ReadKey();
             // Control F5 to run and pause automatically
+
+            Employee employee = new Employee("Dave", "Barnes", 537.00m);
+            Employee employee2 = new Employee("Joe", "Somebody", 125.50m);
+
+            Console.WriteLine(employee.GetFullName());
+            Console.WriteLine(employee.ToString());
         }
     }
 }
